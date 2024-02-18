8c14234190411b25b34c10463119ead1542bc71c
Eli Armstrong <138740082+earmstrong2002@users.noreply.github.com>
2024-02-18 11:58:13 -0800

    MouseFollower constructor now accepts a sprite instead of a radius; setters and getters added.

--- src/main/java/MouseFollower.java
+++ src/main/java/MouseFollower.java
@@ -1,13 +1,33 @@
-import java.awt.*;
+import org.jetbrains.annotations.NotNull;
 
 public class MouseFollower implements GObject {
-  private Circle sprite;
+  private GSprite sprite;
+  private double acceleration;
+  private int deadZoneRadius;
 
-  public MouseFollower(int radius) {
-    sprite = new Circle(radius, new Point());
+  public MouseFollower(GSprite sprite, int acceleration, int deadZoneRadius) {}
+
+  public GSprite getSprite() {
+    return sprite;
   }
 
-  public GShape getSprite() {
-    return sprite;
+  public void setSprite(@NotNull GSprite sprite) {
+    this.sprite = sprite;
+  }
+
+  public double getAcceleration() {
+    return acceleration;
+  }
+
+  public void setAcceleration(int acceleration) {
+    this.acceleration = acceleration;
+  }
+
+  public int getDeadZoneRadius() {
+    return deadZoneRadius;
+  }
+
+  public void setDeadZoneRadius(int deadZoneRadius) {
+    this.deadZoneRadius = deadZoneRadius;
   }
 }

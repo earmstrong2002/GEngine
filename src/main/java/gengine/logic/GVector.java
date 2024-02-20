81f5204f869450a0de1c2ab606e424651d89d370
Eli Armstrong <138740082+earmstrong2002@users.noreply.github.com>
2024-02-20 13:21:58 -0800

    GVector now operates differently internally, allowing us to easily add more things!

--- src/main/java/gengine/logic/GVector.java
+++ src/main/java/gengine/logic/GVector.java
@@ -2,83 +2,25 @@ package gengine.logic;
 
 /** The GVector class represents a vector with magnitude and direction. */
 public class GVector {
-  /** The magnitude of the vector. */
-  private double magnitude;
-
-  /**
-   * The direction of the vector, in radians. Angle progresses clockwise from 0 being directly east.
-   */
-  private double direction;
+  private double x;
+  private double y;
 
   /** Constructs a GVector with default values of zero for both magnitude and direction. */
   public GVector() {
     this(0.0, 0.0);
   }
 
-  /**
-   * Constructs a GVector with the given magnitude and direction values.
-   *
-   * @param magnitude magnitude of the vector. Cannot be negative.
-   * @param direction direction of the vector, in radians.
-   */
-  public GVector(double magnitude, double direction) {
-    setMagnitude(magnitude);
-    setDirection(direction);
+  public GVector(double x, double y) {
+    setX(x);
+    setY(y);
   }
 
-  /**
-   * Retrieves the magnitude of this vector.
-   *
-   * @return the magnitude of this vector.
-   */
-  public double getMagnitude() {
-    return magnitude;
+  private void setX(double x) {
+    this.x = x;
   }
 
-  /**
-   * Sets the magnitude of this vector to the given value.
-   *
-   * @param magnitude new magnitude of the vector. Must be non-negative.
-   */
-  public void setMagnitude(double magnitude) {
-    checkMagnitude(magnitude);
-    this.magnitude = magnitude;
-  }
-
-  /**
-   * Retrieves the magnitude of the x component of this vector.
-   *
-   * @return the magnitude of the x component of this vector.
-   */
-  public double getXMagnitude() {
-    return magnitude * Math.cos(direction);
-  }
-
-  /**
-   * Retrieves the magnitude of the y component of this vector.
-   *
-   * @return the magnitude of the y component of this vector.
-   */
-  public double getYMagintude() {
-    return magnitude * Math.sin(direction);
-  }
-
-  /**
-   * Retrieves the direction of this vector.
-   *
-   * @return the direction of this vector, in radians.
-   */
-  public double getDirection() {
-    return direction;
-  }
-
-  /**
-   * Sets the direction of this vector.
-   *
-   * @param direction new direction of the vector, in radians.
-   */
-  public void setDirection(double direction) {
-    this.direction = direction;
+  private void setY(double y) {
+    this.y = y;
   }
 
   /**
@@ -91,5 +33,64 @@ public class GVector {
       throw new IllegalArgumentException(
           "Vector magnitude value " + magnitude + "is not valid because it is negative.");
     }
+  }
+
+  /**
+   * Retrieves the magnitude of the x component of this vector.
+   *
+   * @return the magnitude of the x component of this vector.
+   */
+  public double getXMagnitude() {
+    return x;
+  }  /**
+   * Retrieves the magnitude of this vector.
+   *
+   * @return the magnitude of this vector.
+   */
+
+  public double getMagnitude() {
+    return Math.sqrt((x * x) + (y * y));
+  }
+
+  /**
+   * Retrieves the magnitude of the y component of this vector.
+   *
+   * @return the magnitude of the y component of this vector.
+   */
+  public double getYMagintude() {
+    return y;
+  }
+  /**
+   * Sets the magnitude of this vector to the given value.
+   *
+   * @param magnitude new magnitude of the vector. Must be non-negative.
+   */
+
+  public void setMagnitude(double magnitude) {
+    double direction = getDirection();
+    setX(magnitude * Math.cos(direction));
+    setY(magnitude * Math.sin(direction));
+  }
+
+
+  /**
+   * Retrieves the direction of this vector.
+   *
+   * @return the direction of this vector, in radians.
+   */
+
+  public double getDirection() {
+    return Math.atan2(y, x);
+  }
+
+  /**
+   * Sets the direction of this vector.
+   *
+   * @param direction new direction of the vector, in radians.
+   */
+  public void setDirection(double direction) {
+    double magnitude = getMagnitude();
+    setX(magnitude * Math.cos(direction));
+    setY(magnitude * Math.sin(direction));
   }
 }

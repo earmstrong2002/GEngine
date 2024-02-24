738c7ba219502891dccfd5fc86c128993361368d
Eli Armstrong <138740082+earmstrong2002@users.noreply.github.com>
2024-02-23 16:49:44 -0800

    Connection line weight now reflects stroke width instead of being constant one pixel; connection line is now rendered below the circle

--- src/main/java/gengine/mouse_follower/rubber_band_mouse_follower/RubberBandMouseFollowerSprite.java
+++ src/main/java/gengine/mouse_follower/rubber_band_mouse_follower/RubberBandMouseFollowerSprite.java
@@ -1,9 +1,9 @@
 package gengine.mouse_follower.rubber_band_mouse_follower;
 
 import gengine.GEngine;
-import gengine.drawable.Circle;
-import gengine.drawable.GSprite;
 import gengine.logic.GPoint;
+import gengine.visuals.Circle;
+import gengine.visuals.GSprite;
 import java.awt.*;
 import org.jetbrains.annotations.NotNull;
 
@@ -29,14 +29,18 @@ public class RubberBandMouseFollowerSprite implements GSprite {
    */
   @Override
   public void draw(@NotNull Graphics graphics) {
-    head.draw(graphics);
+    assert graphics instanceof Graphics2D;
     if (isDrawConnection()) {
+      graphics.setColor(head.getStrokeColor());
+      int strokeWeight = GEngine.metersToPixels(head.getStrokeRadius() - head.getRadius());
+      ((Graphics2D) graphics).setStroke(new BasicStroke(strokeWeight));
       graphics.drawLine(
           GEngine.metersToPixels(position.x),
           GEngine.metersToPixels(position.y),
           GEngine.metersToPixels(mousePosition.x),
           GEngine.metersToPixels(mousePosition.y));
     }
+    head.draw(graphics);
   }
 
   public boolean isDrawConnection() {

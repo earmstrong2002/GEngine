e35883e4fe3fbbd147bb3d21735495c5c0e3004e
Eli Armstrong <138740082+earmstrong2002@users.noreply.github.com>
2024-02-23 16:45:39 -0800

    GColors class will contain a whole lot of color constants; here we have just one pallet.

---
+++ src/main/java/gengine/visuals/GColors.java
@@ -0,0 +1,11 @@
+package gengine.visuals;
+
+import java.awt.*;
+
+public class GColors {
+  public static final Color PAKISTAN_GREEN = Color.decode("#134611");
+  public static final Color INDIA_GREEN = Color.decode("#3e8914");
+  public static final Color PIGMENT_GREEN = Color.decode("#3DA35D");
+  public static final Color LIGHT_GREEN = Color.decode("#96E072");
+  public static final Color NYANZA = Color.decode("#E8FCCF");
+}
